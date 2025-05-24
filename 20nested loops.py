0b90c44e26586d8cce8680fdaf3f6f0a21e41090
Yashwanth M <yashwanthm2584@gmail.com>
2025-05-24 18:01:20 +0530

    Update 20nested loops.py

--- 20nested loops.py
+++ 20nested loops.py
@@ -1,4 +1,22 @@
 # (02:17:28) 
 
 # nested loops
+# A loop within an another loop
 
+# for i  in range(1,11):
+#     print(i,end=" ") 
+
+# Nested for loop
+# for h in range(1,4):
+#     for i  in range(1,11):
+#         print(i,end=" ") 
+#     print("")
+
+# Program to print a rectangle
+# rows = int(input("Enter nimber of rows:"))
+# column = int(input("Enter nimber of columns:"))
+# symbol = input("Enter a symbol to use:")
+# for h in range(rows):
+#     for i  in range(column):
+#         print(symbol,end=" ") 
+#     print("")
